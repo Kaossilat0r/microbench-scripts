4e47ce6dd43fbf5f5cd7b4f08430dded93c58cc5
roman <kaossilat0r.rn@gmail.com>
2016-05-14 23:59:58 +0200

    PEP 8 conventions

--- jsonData.py
+++ jsonData.py
@@ -1,40 +1,39 @@
-'''
+"""
 Created on 08.05.2016
 
 @author: roman
-'''
+"""
 
 import json
 import glob
 
 
-def testJson():
-    exampleDict = {
+def test_json():
+    example_dict = {
         "astar": {"reftime": 200, "ovComp": 250, "PROF": 500,
                   "phase-instrument": {"instrOv": 20, "sampleOv": 10, "overallOv": 30}},
         "libquantum": {"reftime": 210, "ovComp": 220, "PROF": 700},
     }
 
     with open('example.json', 'w') as outFile:
-        json.dump(exampleDict, outFile, indent=1)
+        json.dump(example_dict, outFile, indent=1)
 
     with open('example.json') as inFile:
-        exampleDict = json.load(inFile)
+        example_dict = json.load(inFile)
 
-    print(exampleDict)
+    print(example_dict)
 
 
 # benchmarks -> phases -> ovPercent/Seconds
-def parseBenchmarkResults():
-    benchmarks = []
+def parse_benchmark_results():
 
     for filename in glob.iglob('spec-output-stats/*.log'):
 
-        inFile = open(filename)
-        for line in inFile:
+        in_file = open(filename)
+        for line in in_file:
             if "###" in line:
-                benchmarkName = line.split()[1]
-                benchmark = {PHASES: {}, NAME: benchmarkName}
+                benchmark_name = line.split()[1]
+                benchmark = {PHASES: {}, NAME: benchmark_name}
                 benchmarks.append(benchmark)
                 phases = benchmark[PHASES]
 
@@ -45,25 +44,26 @@ def parseBenchmarkResults():
                 benchmark[COMP] = line.split()[4]
 
             if "==" in line:
-                phaseName = line.split('=')[2]
+                phase_name = line.split('=')[2]
 
             if "---->" in line:
                 phase = {}
-                ovPercent = line.split()[1]
-                ovSeconds = line.split()[-2]
-                phase["percent"] = ovPercent
-                phase["seconds"] = ovSeconds
-                phases[phaseName] = phase
+                ov_percent = line.split()[1]
+                ov_seconds = line.split()[-2]
+                phase["percent"] = ov_percent
+                phase["seconds"] = ov_seconds
+                phases[phase_name] = phase
 
     print(benchmarks)
     return benchmarks
 
 
+NAME, REF, PROF, COMP, PHASES = "name", "refTime", "profTime", "compTime", "phases"
 if __name__ == '__main__':
-    NAME, REF, PROF, COMP, PHASES = "name", "refTime", "profTime", "compTime", "phases"
 
     # 	testJson()
-    benchmarks = parseBenchmarkResults()
+    benchmarks = []
+    benchmarks = parse_benchmark_results()
 
     with open('example.json', 'w') as outFile:
         json.dump(benchmarks, outFile, indent=1, sort_keys=True)
