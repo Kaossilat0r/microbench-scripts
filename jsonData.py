dce8d5d4f0222ebb741d15ac529aa91a8511af32
roman <kaossilat0r.rn@gmail.com>
2016-05-14 23:37:18 +0200

    bye bye, tabs

--- jsonData.py
+++ jsonData.py
@@ -7,62 +7,63 @@ Created on 08.05.2016
 import json
 import glob
 
+
 def testJson():
-	exampleDict = {
-		"astar" : {"reftime" : 200, "ovComp" : 250, "PROF" : 500, "phase-instrument" : {"instrOv" : 20, "sampleOv" : 10, "overallOv" : 30} },
-		"libquantum" : {"reftime" : 210, "ovComp" : 220, "PROF" : 700},
-	}
-	
-	with open('example.json','w') as outFile:
-		json.dump(exampleDict, outFile, indent=1)
-		
-	with open('example.json') as inFile:
-		exampleDict = json.load(inFile)
-	
-	print(exampleDict)
+    exampleDict = {
+        "astar": {"reftime": 200, "ovComp": 250, "PROF": 500,
+                  "phase-instrument": {"instrOv": 20, "sampleOv": 10, "overallOv": 30}},
+        "libquantum": {"reftime": 210, "ovComp": 220, "PROF": 700},
+    }
+
+    with open('example.json', 'w') as outFile:
+        json.dump(exampleDict, outFile, indent=1)
+
+    with open('example.json') as inFile:
+        exampleDict = json.load(inFile)
+
+    print(exampleDict)
+
 
 # benchmarks -> phases -> ovPercent/Seconds
 def parseBenchmarkResults():
-	benchmarks = []
-	
-	for filename in glob.iglob('spec-output-stats/*.log'):
-	
-		inFile = open(filename)
-		for line in inFile:
-			if "###" in line:
-				benchmarkName = line.split()[1]
-				benchmark = {PHASES:{}, NAME: benchmarkName}
-				benchmarks.append(benchmark)
-				phases = benchmark[PHASES]
-	
-			if "runtime:" in line:
-				benchmark[REF] = line.split()[4]
-				benchmark[PROF] = line.split()[1]
-			if "new runtime" in line:
-				benchmark[COMP] = line.split()[4]
-				
-			if "==" in line:
-				phaseName = line.split('=')[2]
-				
-			if "---->" in line:
-				phase = {}
-				ovPercent = line.split()[1]
-				ovSeconds = line.split()[-2]
-				phase["percent"] = ovPercent
-				phase["seconds"] = ovSeconds
-				phases[phaseName] = phase
-			
-	print(benchmarks)
-	return benchmarks
+    benchmarks = []
+
+    for filename in glob.iglob('spec-output-stats/*.log'):
+
+        inFile = open(filename)
+        for line in inFile:
+            if "###" in line:
+                benchmarkName = line.split()[1]
+                benchmark = {PHASES: {}, NAME: benchmarkName}
+                benchmarks.append(benchmark)
+                phases = benchmark[PHASES]
+
+            if "runtime:" in line:
+                benchmark[REF] = line.split()[4]
+                benchmark[PROF] = line.split()[1]
+            if "new runtime" in line:
+                benchmark[COMP] = line.split()[4]
+
+            if "==" in line:
+                phaseName = line.split('=')[2]
+
+            if "---->" in line:
+                phase = {}
+                ovPercent = line.split()[1]
+                ovSeconds = line.split()[-2]
+                phase["percent"] = ovPercent
+                phase["seconds"] = ovSeconds
+                phases[phaseName] = phase
+
+    print(benchmarks)
+    return benchmarks
+
 
 if __name__ == '__main__':
-	
-	NAME, REF, PROF, COMP, PHASES = "name", "refTime", "profTime", "compTime", "phases"
-	
-# 	testJson()
-	benchmarks = parseBenchmarkResults()
+    NAME, REF, PROF, COMP, PHASES = "name", "refTime", "profTime", "compTime", "phases"
 
-	with open('example.json','w') as outFile:
-		json.dump(benchmarks, outFile, indent=1, sort_keys=True)
-	
-	
+    # 	testJson()
+    benchmarks = parseBenchmarkResults()
+
+    with open('example.json', 'w') as outFile:
+        json.dump(benchmarks, outFile, indent=1, sort_keys=True)
