5d8614dacebfa8eba8c353c7f4d403dc48402028
roman <kaossilat0r.rn@gmail.com>
2016-07-14 04:54:40 +0200

    table in adjustbox

--- estimate/ovCompensation.py
+++ estimate/ovCompensation.py
@@ -298,16 +298,16 @@ def figure_vs_phase(vs_phases_names, max_y=50, fig_width=1.0, fig_ratio=1/3, adj
 
 def create_latex_table_vs(benchmark_names, filename, vs_phases, vs_phases_names):
     with open("../"+ rel_thesis_table_dir + filename + ".tex", 'w') as out:
-        # out.write("\\begin{adjustbox}{max width=\\textwidth,center}\n")
+        out.write("\\begin{adjustbox}{max width=1\\textwidth,center}\n")
         out.write("\\begin{tabular}{ " + "".join("c" for x in range(len(benchmark_names) + 1)) + " }\n")
         out.write("\\hline\n")
-        out.write(" & " + " & ".join("\\rot{" + b.split('.')[-1] + "}" for b in benchmark_names) + " \\\\ \\hline\n")
+        out.write(" & " + " & ".join("\\rot{" + b.split('.')[-1] + "\\,}" for b in benchmark_names) + " \\\\ \\hline\n")
         for phase_name in vs_phases_names:
             out.write(phase_name + " & " + " & ".join(
                 "{0:.1f}".format(p) for p in vs_phases[phase_name][C.PERCENT]) + " \\\\\n")
         out.write("\\hline\n")
         out.write("\\end{tabular}\n")
-        # out.write("\\end{adjustbox}\n")
+        out.write("\\end{adjustbox}\n")
 
 
 def autolabel(plt, rects, above_figure=True):
